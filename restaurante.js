e4b684e5c3b6d9a91c1e16380d13d48736978af5
Carmen <carmenverdu91@gmail.com>
2023-09-12 13:07:06 +0200

    Ejercicio practico finalizado

---
+++ restaurante.js
@@ -0,0 +1,8 @@
+var totalComida = 2800 + 1300;
+var totalBebida = 900 + 650;
+
+var total = totalComida + totalBebida;
+
+console.log ("El total de la cuenta de la comida es: " + totalComida);
+console.log ("El total de la cuenta de la bebida es: " + totalBebida);
+console.log ("El total completo de la cena es: " + total);
